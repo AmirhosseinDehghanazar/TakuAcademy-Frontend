d71193ff83483781f8bdfdb38552fcf03a515625
mohamad amin <mohamadeaminn@gmail.com>
2023-09-07 17:23:47 +0400

    configure react-router-dom i could have done it later but i did it now idkw

---
+++ TakuAcademy-Frontend/src/layout/Main.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../components/main/navbar/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default Main;
---
+++ TakuAcademy-Frontend/src/router/router.jsx
@@ -0,0 +1,5 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import App from "../App";
+
+export const router = createBrowserRouter([{ path: "/", element: <App /> }]);

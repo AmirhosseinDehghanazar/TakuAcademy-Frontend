956f343fc7401544e0f010acf9f475f9b15d737e
mohamad amin <mohamadeaminn@gmail.com>
2023-09-09 19:47:12 +0400

    question component done full responsive but i will make sure of it did not test it fully yeah that's it

--- TakuAcademy-Frontend/src/components/main/question/Question.jsx
+++ TakuAcademy-Frontend/src/components/main/question/Question.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import "./question.css";
-import icon1 from "../../../assets/question/icon1.png";
-import icon2 from "../../../assets/question/icon2.png";
-import icon3 from "../../../assets/question/icon3.png";
-import icon4 from "../../../assets/question/icon4.png";
-import icon5 from "../../../assets/question/icon5.png";
-import icon6 from "../../../assets/question/icon6.png";
-import underline from "../../../assets/question/qVector.png";
 import question from "../../../assets/question/question.png";
 
 const Question = () => {
   return (
     <div className="flex mar-x flex-col-reverse question-parent justify-center items-center lg:flex-row my-24">
       {/* image element */}
-      <div className="mx-auto">
+      <div className="">
         <img
           className="object-cover transition-all md:max-w-lg xl:max-w-2xl"
           src={question}
@@ -23,34 +16,39 @@ const Question = () => {
       {/* question and reasons */}
       <div className="flex flex-col justify-center gap-5">
         {/* why taku */}
-        <div>
-          <div className="font-bold text-4xl">چرا تاکو آکادمی؟</div>
-          <div className="underLine" />
+        <div className="flex flex-col w-52">
+          <div className="font-bold text-3xl">چرا تاکو آکادمی؟</div>
+          <div className="bg-under w-44 h-2" />
         </div>
 
         {/* reasons */}
-        <div>
-          <div className="bg-img bg-dart" />
+        <div className="flex justify-start items-center gap-4">
+          <div className="bg-all bg-dart p-6 border rounded-full" />
           <div>چون نمیزاره شما گیج بشید و یا راهی که دارید رو گم کنید.</div>
         </div>
-        <div>
-          <div className="bg-img bg-check" />
+
+        <div className="flex justify-start items-center gap-4">
+          <div className="bg-all bg-check  p-6 border rounded-full" />
           <div>۰ تا ۱۰۰ هر حوزه‌ای رو بدون پیش نیاز بهتون یاد میده.</div>
         </div>
-        <div>
-          <div className="bg-img bg-up" />
+
+        <div className="flex justify-start items-center gap-4">
+          <div className="bg-all bg-up  p-6 border rounded-full" />
           <div>با توجه به عملکرد شما رایگان بهتون فضای پیشرفت داده میشه.</div>
         </div>
-        <div>
-          <div className="bg-img bg-per" />
+
+        <div className="flex justify-start items-center gap-4">
+          <div className="bg-all bg-per  p-6 border rounded-full" />
           <div>چون یا رایگانه یا خیلی ارزون :))</div>
         </div>
-        <div>
-          <div className="bg-img bg-person" />
+
+        <div className="flex justify-start items-center gap-4">
+          <div className="bg-all bg-person  p-6 border rounded-full" />
           <div>چون بهترین اساتید رو برای تدریس در اختیارتون قرار میده.</div>
         </div>
-        <div>
-          <div className="bg-img bg-chat" />
+
+        <div className="flex justify-around items-center gap-4">
+          <div className="bg-all bg-chat  p-6 border rounded-full" />
           <div>
             چون پشتیبان‌های ما همیشه پیگیر روند شما و راهنمایی شما هستن.
           </div>

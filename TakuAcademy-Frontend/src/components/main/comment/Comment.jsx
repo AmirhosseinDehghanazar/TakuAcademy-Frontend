c610f150e5c1e0ca45c2b13f2d2078a54a288c08
mohamad amin <mohamadeaminn@gmail.com>
2023-09-10 17:07:47 +0400

    few changes to remember what to add

--- TakuAcademy-Frontend/src/components/main/comment/Comment.jsx
+++ TakuAcademy-Frontend/src/components/main/comment/Comment.jsx
@@ -16,35 +16,29 @@ const Comment = () => {
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
           از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون.
         </div>
-        <button className=" bg-gradient flex justify-center items-center gap-2 py-3 px-5 rounded-lg max-w-xs">
+        <button className="arrow-btn bg-gradient flex justify-center items-center gap-2 py-3 px-5 rounded-lg max-w-xs">
           <p> همین حالا شروع کنید</p>
           <div className="bg-arrow bg-arrow-w"></div>
         </button>
       </div>
       {/* comments */}
-      <div className="relative mb-20">
+      <div className="relative">
         {/* blueCube */}
-        <div className="h-80 thin-border max-w-2xl rounded-lg bg-cusDarkBlue rounded-20">
+        <div className="h-80 thin-border max-w-2xl p-5 rounded-lg bg-cusDarkBlue rounded-20">
           {/* this div contains texts and manage them the texts on top */}
           <div className="flex justify-between items-center  mx-3 md:mx-6">
             <div className="flex flex-col">
-              <div className="flex items-center gap-2 py-3">
-                <div className="bg-flash"></div>
-                <p className=" text-xl">دسته بندی دوره ها</p>
+              <div className="flex justify-between items-center">
+                <p className=" text-xl">commenter information </p>{" "}
+                {/* rating */}
+                <div>react-rating</div>
               </div>
 
-              <p className="mb-4 text-sm tracking-wider max-w-lg lg:max-w-2xl ">
+              <div>
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-                استفاده از طراحان گرافیک است.
-              </p>
+                استفاده از طراحان گرافیک است."react-slick"
+              </div>
             </div>
-            {/* this bitch only shows up in tablet or higher screens as figma showed */}
-            <a href="#">
-              <button className="arrow-btn">
-                مشاهده دوره‌ها
-                <span className="background-props bg-arrow bg-arrow-w"></span>
-              </button>
-            </a>
           </div>
           {/* these two element below me are the two lightBlue divs behindcomment */}
           <div className="h-80 shy-div-comment -rotate-3 opacity-70 rounded-lg" />
